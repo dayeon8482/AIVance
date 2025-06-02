e012ba1771e8faa07df086c9df5c8ec590d933ba
dayeon8482 <da8482@naver.com>
2025-06-02 18:34:55 +0900

    탑바, 메엔 1 완성

--- script/script.js
+++ script/script.js
@@ -1,10 +1,38 @@
 console.clear();
+//풀페이지
+new fullpage("#fullpage", {
+  autoScrolling: false,
+  scrollOverflow: false,
+});
+//커서 포인터
 document.addEventListener("mousemove", (e) => {
   const cursor = document.querySelector(".custom-cursor");
   cursor.style.left = e.clientX + "px";
   cursor.style.top = e.clientY + "px";
 });
-new fullpage("#fullpage", {
-  autoScrolling: true,
-  scrollOverflow: false,
-});
+//메뉴
+function menuBtn_init() {
+  $(".top-bar-menu-btn").click(function () {
+    $(".menu-wrap").toggleClass("active");
+    $(this).toggleClass("active");
+
+    if (!$(".top-bar-menu-btn").hasClass("open")) {
+      $(".menu > li").removeClass("active"); // ← 서브메뉴 모두 닫기
+    }
+  });
+}
+menuBtn_init();
+
+// 서브메뉴
+function subMenu_init() {
+  $(".menu > li").click(function () {
+    let $this = $(this);
+
+    const $menuBox = $this.closest(".menu");
+    const $subMenu = $menuBox.find(".sub-menu");
+
+    $this.siblings(".active").removeClass("active");
+    $this.addClass("active");
+  });
+}
+subMenu_init();
